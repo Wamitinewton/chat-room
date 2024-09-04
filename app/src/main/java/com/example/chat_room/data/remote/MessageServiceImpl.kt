f07f38c0b233f3f0173cb76da4cd87a3fc7c4b26
Wamitinewton <wamitinewton@gmail.com>
2024-09-04 14:32:56 +0300

    fixed a client builder error

--- app/src/main/java/com/example/chat_room/data/remote/MessageServiceImpl.kt
+++ app/src/main/java/com/example/chat_room/data/remote/MessageServiceImpl.kt
@@ -3,16 +3,22 @@ package com.example.chat_room.data.remote
 import com.example.chat_room.data.remote.dto.MessageDto
 import com.example.chat_room.domain.model.Message
 import io.ktor.client.HttpClient
+import io.ktor.client.call.body
 import io.ktor.client.request.get
+import io.ktor.client.request.url
 
 class MessageServiceImpl(
     private val client: HttpClient
-): MessageService{
+) : MessageService {
 
     override suspend fun getAllMessage(): List<Message> {
         return try {
-            client.get<List<MessageDto>>(MessageService.EndPoints.GetAllMessages.url)
-        }catch (e: Exception){
+            client.get {
+                url(MessageService.EndPoints.GetAllMessages.url)
+            }.body<List<MessageDto>>()
+                .map { it.toMessage() }
+
+        } catch (e: Exception) {
             emptyList()
         }
     }
