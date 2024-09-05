9d9a41f811509a184b2323b20476c4f7f6ebd7c7
Wamitinewton <wamitinewton@gmail.com>
2024-09-05 11:02:30 +0300

    hilt module injection

---
+++ app/src/main/java/com/example/chat_room/di/AppModule.kt
@@ -0,0 +1,47 @@
+package com.example.chat_room.di
+
+import com.example.chat_room.data.remote.ChatSocketService
+import com.example.chat_room.data.remote.ChatSocketServiceImpl
+import com.example.chat_room.data.remote.MessageService
+import com.example.chat_room.data.remote.MessageServiceImpl
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.features.json.JsonFeature
+import io.ktor.client.features.json.serializer.KotlinxSerializer
+import io.ktor.client.features.logging.Logging
+import io.ktor.client.features.websocket.WebSockets
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideHttpClient(): HttpClient {
+        // CIO is basically Coroutine-Based IO
+        return HttpClient(CIO) {
+            install(Logging)
+            install(WebSockets)
+            install(JsonFeature) {
+                serializer = KotlinxSerializer()
+            }
+        }
+    }
+
+    @Provides
+    @Singleton
+    fun provideMessageService(client: HttpClient): MessageService {
+        return MessageServiceImpl(client)
+    }
+
+    @Provides
+    @Singleton
+    fun provideChatSocketService(client: HttpClient): ChatSocketService {
+        return ChatSocketServiceImpl(client)
+    }
+}
